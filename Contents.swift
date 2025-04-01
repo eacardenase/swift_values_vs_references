922ba33f69aa5e72f976c283026b59dd2151c1db
Edwin Cardenas <ecardenas@lwolf.com>
2025-03-31 19:33:33 -0500

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,38 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+var playgroundGreeting = greeting
+
+playgroundGreeting += "! How are you today?"
+
+greeting
+
+class Employee {
+    var id: Int = 0
+}
+
+let anika = Employee()
+let theBoss = anika
+
+anika.id = 16
+anika.id
+theBoss.id
+
+struct Company {
+    var boss: Employee
+}
+
+let acme = Company(boss: anika)
+
+let mel = Employee()
+//acme.boss = mel // error, acme is constant
+
+mel.id = 86 // a constant referece type can change its variables properties
+
+acme.boss.id
+
+let widgetCo = acme
+
+anika.id = 12
+
+widgetCo.boss.id
---
+++ README.md
@@ -0,0 +1,1 @@
+# Value vs Reference Types
